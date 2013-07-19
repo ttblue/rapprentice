0a76b86e74a0046735afc377831a273839a745ea
Sibi Venkatesan <sibi.venkatesan@gmail.com>
2013-07-18 22:39:17 -0700

    Test for ar_service added

--- suturing/test_ar_service.py
+++ suturing/test_ar_service.py
@@ -4,15 +4,24 @@ from ar_track_service.srv import MarkerPositions, MarkerPositionsRequest, Marker
 from ar_track_alvar.msg import AlvarMarkers
 from sensor_msgs.msg import PointCloud2
 
+class TopicListener(object):
+    "stores last message"
+    last_msg = None
+    def __init__(self,topic_name,msg_type):
+        self.sub = rospy.Subscriber(topic_name,msg_type,self.callback)        
+
+        rospy.loginfo('waiting for the first message: %s'%topic_name)
+        while self.last_msg is None: rospy.sleep(.01)
+        rospy.loginfo('ok: %s'%topic_name)
+
+    def callback(self,msg):
+        self.last_msg = msg
+
+
+pcL = TopicListener('/camera/depth_registered/points', PointCloud2)
 getMarkers = rospy.ServiceProxy('getMarkers', MarkerPositions)
 
-
-pc = PointCloud2()
-req = MarkerPositionsRequest()
-req.pc = pc
-
-print req
-
-resp = getMarkers(req)
-
-print resp
+def getPose ():
+    req = MarkerPositionsRequest()
+    req.pc = pcL.last_msg
+    return getMarkers(req)
