750c012ed4d286b60ed9df3e7f49bc286a609a93
Sibi Venkatesan <sibi.venkatesan@gmail.com>
2013-07-22 15:55:21 -0700

    Latest changes in adding markers and using keypoints.

--- rapprentice/clouds.py
+++ rapprentice/clouds.py
@@ -9,7 +9,10 @@ def depth_to_xyz(depth,f=535.):
     x,y = np.meshgrid(np.arange(640), np.arange(480))
     assert depth.shape == (480, 640)
     XYZ = np.empty((480,640,3))
+    
     Z = XYZ[:,:,2] = depth / 1000. # convert mm -> meters
+    np.putmask(Z, Z==0, np.nan)
+    
     XYZ[:,:,0] = (x - cx)*(Z/f)
     XYZ[:,:,1] = (y - cy)*(Z/f)
 
